9952744b5ca36175d55fe56d15880035353e62fa
Niellai <himura_sg@hotmail.com>
2017-06-29 20:24:07 +0800

    Update main_app
    main_app can now handle schedule calls. When main_app is already running
    will cancel current process.

--- main_app.py
+++ main_app.py
@@ -1,8 +1,29 @@
 # coding: utf-8
-
+import logging
+import socket
+import sys
 import re
 from Tweet import Tweet
 
+lock_socket = None 
+
+def is_lock_free():
+    global lock_socket
+    lock_socket = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
+    try:
+        # this should be unique. using your username as a prefix is a convention
+        lock_id = "niellai.tweetListener"
+        lock_socket.bind('\0' + lock_id)
+        logging.debug("Acquired lock %r" % (lock_id,))
+        return True
+    except socket.error:
+        # socket already locked, task must already be running
+        logging.info("Failed to acquire lock %r" % (lock_id,))
+        return False
+    
+if not is_lock_free():
+    sys.exit()
+    
 tweet = Tweet()
 tweet.listen()
 
