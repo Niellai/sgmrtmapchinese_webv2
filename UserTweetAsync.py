3c0fd1a0cc2309c856a98bbeb1df81b1553cfde6
Niellai <himura_sg@hotmail.com>
2017-07-16 11:11:50 +0800

    Clear bug in user tweet

--- UserTweetAsync.py
+++ UserTweetAsync.py
@@ -71,6 +71,56 @@ class UserTweet:
     def writeToSheet(self, ori_tweet, replaced_tweet):
         exportSheet.writeToSheet3Async(ori_tweet, replaced_tweet)
         
+    # Extract tweet and return json object, return null when not send from targeted user             
+    def extractTweet(self, tweetsStr):
+        try:            
+            jsonData = json.loads(tweetsStr)                        
+            
+            # Ignore retweet
+            if jsonData['retweeted'] == True:
+                print('Retweet ignored')
+                return
+            
+            # Extracting userID
+            data = {}                        
+            userData = jsonData['user']                        
+            userID = userData['id_str']
+            if userID == '3087502272':
+                data['text'] = "[Bus service]{}".format(jsonData['text'])
+            elif userID == '307781209' or userID == '80337313':
+                data['text'] = jsonData['text']
+            else:     
+                print('Ignore current tweet')
+                return
+            
+            # Extracting entities, for media url         
+            try:
+                if 'extended_tweet' in jsonData:
+                    extTweetData = jsonData['extended_tweet']
+                    entitiesData = extTweetData['entities']
+                    mediaData = entitiesData['media'][0]
+                    media_url = mediaData['media_url']
+                    if len(media_url) > 0:
+                        data['media_url'] = media_url                    
+            except Exception as e:
+                print('extractTweet > extended_tweet error:{}'.format(e)) 
+                
+            try:
+                if 'entities' in jsonData:
+                    entitiesData = jsonData['entities']
+                    mediaData = entitiesData['media'][0]
+                    media_url = mediaData['media_url']
+                    if len(media_url) > 0:
+                        data['media_url'] = media_url
+            except Exception as e:
+                print('extractTweet > entities error:{}'.format(e))                               
+            
+            data['timestamp_ms'] = int(jsonData['timestamp_ms'])        
+            jsonDataStr = json.dumps(data, ensure_ascii=False)   
+            print("Tweet received: {}".format(data['text']))
+            return json.loads(jsonDataStr)    
+        except Exception as e: 
+            print("ExtractTweet error: {}".format(e))           
 
 
 # In[4]:
